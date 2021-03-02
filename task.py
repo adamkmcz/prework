38f8540c6cd0d9346c553f203663fb3d0da58d5b
Adam <adam.kmcz@gmail.com>
2021-03-02 11:32:08 +0100

    second commit

--- task.py
+++ task.py
@@ -1,1 +1,11 @@
-print("Mam na imię <Adam>")
+print("Mam na    imię <Adam>")
+
+a = 1
+b = 1.2
+c = "abc"
+d = True
+
+print(f'Zmienna a ma wartość {a}')
+print(f'Zmienna b ma wartość {b}')
+print(f'Zmienna c ma wartość {c}')
+print(f'Zmienna d ma wartość {d}')
