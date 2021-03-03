0c355a97fa35871ca01dc83350e29993775a29ca
Adam <adam.kmcz@gmail.com>
2021-03-03 15:53:09 +0100

    Porównywanie zmiennych

--- task.py
+++ task.py
@@ -1,72 +1,77 @@
+#
+# print("Mam na    imię <Adam>")
+#
+# a = 1
+# b = 1.2
+# c = "abc"
+# d = True
+#
+# print(f'Zmienna a ma wartość {a}')
+# print(f'Zmienna b ma wartość {b}')
+# print(f'Zmienna c ma wartość {c}')
+# print(f'Zmienna d ma wartość {d}')
+#
+# foo = True
+# bar = False
+# check = foo == bar
+# print(f'zmienna check ma wartosc {check}')
+#
+# add1 = 3
+# add2 = 1.3
+#
+# result = add1 + add2
+#
+# print(result)
+#
+# a1 = 25
+# a2 = 10
+#
+# sum_value = a1 + a2
+# quotus = a1 / a2
+# int_part = int(quotus)
+# print(int_part)
+# print(f'{a1}\n{a2}\n{sum_value}\n{quotus}\n{int_part}')
+#
+# animals = []
+#
+# animals.append('kot')
+# print(animals)
+# animals.append('panda')
+# print(animals)
+# animals.append('koń')
+# print(animals)
+#
+# numbers = [1, 2, 3, 4, 5, 6, 7, 8]
+# print(numbers[-2])
+#
+# characters = ["Harry", "Ron", "Hermione"]
+#
+# print(characters[0])
+# print(characters[-1])
+#
+# # name = input('your name')
+# # surname = input('your surname')
+# # print(f'{name} {surname} jest programistą Pythona')
+#
+# list=['a','b','c','d','e']
+#
+# concat = ' '.join(list)
+# print(concat)
+#
+# a = 17
+# b = 4
+# result = a % b
+# print(result)
+#
+# counter = 145
+# counter += 1
+# print(counter)
+#
+# counter -= 1
+# print(counter)
 
-print("Mam na    imię <Adam>")
-
-a = 1
-b = 1.2
-c = "abc"
-d = True
-
-print(f'Zmienna a ma wartość {a}')
-print(f'Zmienna b ma wartość {b}')
-print(f'Zmienna c ma wartość {c}')
-print(f'Zmienna d ma wartość {d}')
-
-foo = True
-bar = False
-check = foo == bar
-print(f'zmienna check ma wartosc {check}')
-
-add1 = 3
-add2 = 1.3
-
-result = add1 + add2
-
-print(result)
-
-a1 = 25
-a2 = 10
-
-sum_value = a1 + a2
-quotus = a1 / a2
-int_part = int(quotus)
-print(int_part)
-print(f'{a1}\n{a2}\n{sum_value}\n{quotus}\n{int_part}')
-
-animals = []
-
-animals.append('kot')
-print(animals)
-animals.append('panda')
-print(animals)
-animals.append('koń')
-print(animals)
-
-numbers = [1, 2, 3, 4, 5, 6, 7, 8]
-print(numbers[-2])
-
-characters = ["Harry", "Ron", "Hermione"]
-
-print(characters[0])
-print(characters[-1])
-
-# name = input('your name')
-# surname = input('your surname')
-# print(f'{name} {surname} jest programistą Pythona')
-
-list=['a','b','c','d','e']
-
-concat = ' '.join(list)
-print(concat)
-
-a = 17
+a = 8
 b = 4
-result = a % b
+result = a > b
 print(result)
-
-counter = 145
-counter += 1
-print(counter)
-
-counter -= 1
-print(counter)
 
