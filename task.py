97ac496b64cdda6d269f2b9a0f8e91673445728e
Adam <adam.kmcz@gmail.com>
2021-03-04 16:42:14 +0100

    pętla while

--- task.py
+++ task.py
@@ -1,94 +1,103 @@
+# #
+# # print("Mam na    imię <Adam>")
+# #
+# # a = 1
+# # b = 1.2
+# # c = "abc"
+# # d = True
+# #
+# # print(f'Zmienna a ma wartość {a}')
+# # print(f'Zmienna b ma wartość {b}')
+# # print(f'Zmienna c ma wartość {c}')
+# # print(f'Zmienna d ma wartość {d}')
+# #
+# # foo = True
+# # bar = False
+# # check = foo == bar
+# # print(f'zmienna check ma wartosc {check}')
+# #
+# # add1 = 3
+# # add2 = 1.3
+# #
+# # result = add1 + add2
+# #
+# # print(result)
+# #
+# # a1 = 25
+# # a2 = 10
+# #
+# # sum_value = a1 + a2
+# # quotus = a1 / a2
+# # int_part = int(quotus)
+# # print(int_part)
+# # print(f'{a1}\n{a2}\n{sum_value}\n{quotus}\n{int_part}')
+# #
+# # animals = []
+# #
+# # animals.append('kot')
+# # print(animals)
+# # animals.append('panda')
+# # print(animals)
+# # animals.append('koń')
+# # print(animals)
+# #
+# # numbers = [1, 2, 3, 4, 5, 6, 7, 8]
+# # print(numbers[-2])
+# #
+# # characters = ["Harry", "Ron", "Hermione"]
+# #
+# # print(characters[0])
+# # print(characters[-1])
+# #
+# # # name = input('your name')
+# # # surname = input('your surname')
+# # # print(f'{name} {surname} jest programistą Pythona')
+# #
+# # list=['a','b','c','d','e']
+# #
+# # concat = ' '.join(list)
+# # print(concat)
+# #
+# # a = 17
+# # b = 4
+# # result = a % b
+# # print(result)
+# #
+# # counter = 145
+# # counter += 1
+# # print(counter)
+# #
+# # counter -= 1
+# # print(counter)
 #
-# print("Mam na    imię <Adam>")
-#
-# a = 1
-# b = 1.2
-# c = "abc"
-# d = True
-#
-# print(f'Zmienna a ma wartość {a}')
-# print(f'Zmienna b ma wartość {b}')
-# print(f'Zmienna c ma wartość {c}')
-# print(f'Zmienna d ma wartość {d}')
-#
-# foo = True
-# bar = False
-# check = foo == bar
-# print(f'zmienna check ma wartosc {check}')
-#
-# add1 = 3
-# add2 = 1.3
-#
-# result = add1 + add2
-#
-# print(result)
-#
-# a1 = 25
-# a2 = 10
-#
-# sum_value = a1 + a2
-# quotus = a1 / a2
-# int_part = int(quotus)
-# print(int_part)
-# print(f'{a1}\n{a2}\n{sum_value}\n{quotus}\n{int_part}')
-#
-# animals = []
-#
-# animals.append('kot')
-# print(animals)
-# animals.append('panda')
-# print(animals)
-# animals.append('koń')
-# print(animals)
-#
-# numbers = [1, 2, 3, 4, 5, 6, 7, 8]
-# print(numbers[-2])
-#
-# characters = ["Harry", "Ron", "Hermione"]
-#
-# print(characters[0])
-# print(characters[-1])
-#
-# # name = input('your name')
-# # surname = input('your surname')
-# # print(f'{name} {surname} jest programistą Pythona')
-#
-# list=['a','b','c','d','e']
-#
-# concat = ' '.join(list)
-# print(concat)
-#
-# a = 17
+# a = 8
 # b = 4
-# result = a % b
+# result = a > b
 # print(result)
 #
-# counter = 145
-# counter += 1
-# print(counter)
+# father = 1974
+# child = 2007
+# print(f'Ojciec jest starszy od dziecka o {(child - father)} lat')
 #
-# counter -= 1
-# print(counter)
+# result = round(11 / 7, 2)
+# print(f' 11 : 7 = {result}')
+#
+# name = input('Podaj swoje imię:')
+# print(name)
+# rok_urodzenia = input('Podaj rok urodzenia')
+# print(rok_urodzenia)
+# year = rok_urodzenia
+# age = 2021 - int(rok_urodzenia)
+# print(f'Użytkownik {name} jest w wieku {age} lat')
+#
 
-a = 8
-b = 4
-result = a > b
-print(result)
+i = 1
+while i <= 10 :
+    print(f'Jestem programistą Pythona')
+    i += 1
 
-father = 1974
-child = 2007
-print(f'Ojciec jest starszy od dziecka o {(child - father)} lat')
 
-result = round(11 / 7, 2)
-print(f' 11 : 7 = {result}')
 
-name = input('Podaj swoje imię:')
-print(name)
-rok_urodzenia = input('Podaj rok urodzenia')
-print(rok_urodzenia)
-year = rok_urodzenia
-age = 2021 - int(rok_urodzenia)
-print(f'Użytkownik {name} jest w wieku {age} lat')
 
 
 
